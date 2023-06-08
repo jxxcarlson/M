b5cb3e864f3c9e99633863c6cd5fd692ccd1fb9a
James Carlson <jxxcarlson@gmail.com>
2023-06-08 09:00:51 -0400

    Remove external dependency for loop

--- src/M/ParserHelpers.elm
+++ src/M/ParserHelpers.elm
@@ -28,3 +28,18 @@ nextStep predicate list =
 prependMessage : Int -> String -> List String -> List String
 prependMessage lineNumber message messages =
     (message ++ " (line " ++ String.fromInt lineNumber ++ ")") :: List.take 2 messages
+
+
+type Step state a
+    = Loop state
+    | Done a
+
+
+loop : state -> (state -> Step state a) -> a
+loop s f =
+    case f s of
+        Loop s_ ->
+            loop s_ f
+
+        Done b ->
+            b
