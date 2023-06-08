0df071020f0e9e75639f581a1ee280d65b9c43f9
James Carlson <jxxcarlson@gmail.com>
2023-06-08 09:00:19 -0400

    Make toExpressionBlock (from PrimitiveBlock) generic

--- src/Generic/ExpressionBlockParser.elm
+++ src/Generic/ExpressionBlockParser.elm
@@ -1,17 +1,9 @@
-module M.ExpressionBlockParser exposing (toExpressionBlock)
+module Generic.ExpressionBlockParser exposing (toExpressionBlock)
 
-import Either exposing (Either(..))
+import Generic.Language exposing (ExpressionBlock, Heading(..), PrimitiveBlock)
 import M.ExpressionParser
-import M.Language exposing (ExpressionBlock, PrimitiveBlock, Heading(..)
-import M.PrimitiveBlockParser
 
 
 toExpressionBlock : Int -> PrimitiveBlock -> ExpressionBlock
 toExpressionBlock lineNumber block =
-    case block.heading of
-        Paragraph ->
-             {block | body = Right <| M.ExpressionParser.parse lineNumber (String.join "\n" block.lines)}
-        Ordinary _ ->
-            {block | body = Right <| M.ExpressionParser.parse lineNumber (String.join "\n" block.lines)}
-        Verbatim _ ->
-            {block | body = Left <| (String.join "\n" block.lines)}
+    Generic.Language.toExpressionBlock (M.ExpressionParser.parse lineNumber) block
